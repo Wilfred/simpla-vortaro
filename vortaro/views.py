ed76bfe0c894377de893d6758fea07f152b07b7c
Wilfred <me@wilfred.me.uk>
2010-09-18 11:55:15 +0100

    basic word searching

--- vortaro/views.py
+++ vortaro/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse
 from django.template import Context, loader
 
+from projektoj.vortaro.models import Word
+
 def index(request):
     template = loader.get_template('vortaro/index.html')
     context = Context({})
     return HttpResponse(template.render(context))
 
 def search_word(request, word):
+    matching_words = [w.word for w in Word.objects.filter(word=word)]
+
     template = loader.get_template('vortaro/word.html')
-    context = Context({'word':word})
+    context = Context({'search_term':word, 'matching_words':matching_words})
     return HttpResponse(template.render(context))
