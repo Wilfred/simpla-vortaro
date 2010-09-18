3007551f4ed47d230fb89dc9c1e4c507169fbea7
Wilfred <me@wilfred.me.uk>
2010-09-18 12:58:23 +0100

    making URLs regexs far more permissive so we can have ŭ etc as input

--- urls.py
+++ urls.py
@@ -6,14 +6,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'projektoj.vortaro.views.index'),
-    (r'^serchi/(?P<word>[a-zA-Z]*)$', 'projektoj.vortaro.views.search_word'),
-    (r'^(?P<word>[a-zA-Z]+)$', 'projektoj.vortaro.views.view_word'),
-    # Example:
-    # (r'^projektoj/', include('projektoj.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^serchi/(?P<word>.*)$', 'projektoj.vortaro.views.search_word'),
+    (r'^(?P<word>.*)$', 'projektoj.vortaro.views.view_word'),
 )
