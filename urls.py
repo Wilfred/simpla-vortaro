15a89b6a31d775a58bc24e86284bbc2cc705a3a6
Wilfred <me@wilfred.me.uk>
2010-09-26 22:55:50 +0100

    changing page structure so that the general website structure can be navigated

--- urls.py
+++ urls.py
@@ -1,11 +1,8 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    (r'^$', 'projektoj.vortaro.views.index'),
-    (r'^serchi/(?P<word>.*)$', 'projektoj.vortaro.views.search_word'),
-    (r'^(?P<word>.*)$', 'projektoj.vortaro.views.view_word'),
+    (u'^$', 'projektoj.vortaro.views.index'),
+    (u'^serĉi$', 'projektoj.vortaro.views.search_word'),
+    (u'^vorto/(?P<word>.*)$', 'projektoj.vortaro.views.view_word'),
 )
--- vortaro/views.py
+++ vortaro/views.py
@@ -1,15 +1,16 @@
 from django.http import HttpResponse
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
+from django.shortcuts import render_to_response
 
 from projektoj.vortaro.models import Word, Variant
 from spelling import get_spelling_variations
 
 def index(request):
-    template = loader.get_template('vortaro/index.html')
-    context = Context({})
-    return HttpResponse(template.render(context))
+    return render_to_response('vortaro/index.html', {},
+                              context_instance=RequestContext(request))
 
-def search_word(request, word):
+def search_word(request):
+    word = request.POST['vorto']
     # find all words that match this search term, regardless of variant
     matching_variants = Variant.objects.filter(variant=word)
     matching_words = []
@@ -25,11 +26,11 @@ def search_word(request, word):
         if (not variant.word in similar_words) and (not variant.word in matching_words):
             similar_words.append(variant.word)
 
-    template = loader.get_template('vortaro/search.html')
     context = Context({'search_term':word,
                        'matching_words':matching_words,
                        'similar_words':similar_words})
-    return HttpResponse(template.render(context))
+    return render_to_response('vortaro/search.html', context,
+                              context_instance=RequestContext(request))
 
 def view_word(request, word):
     template = loader.get_template('vortaro/word.html')
