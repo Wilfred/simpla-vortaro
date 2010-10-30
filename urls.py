89461160914aef370d59e665a19caeb482d2bc2e
Wilfred <me@wilfred.me.uk>
2010-10-30 23:56:21 +0100

    setting Django to serve static files in debug, fixing theme bugs exposed as a result

--- urls.py
+++ urls.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
+from django.conf import settings
 
-urlpatterns = patterns('',
-    (u'^.*$', 'vortaro.views.index'),
-)
+if settings.DEBUG:
+    # serve static files using Django
+    urlpatterns = patterns('',
+        (r'^resources/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/wilfred/html/projektoj/static'}),
+        (u'^.*$', 'vortaro.views.index'),
+                           )
+else:
+    urlpatterns = patterns('',
+        (u'^.*$', 'vortaro.views.index'),)
+
