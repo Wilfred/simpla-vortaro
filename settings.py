a9c0d384db026de37c07380a1524cb00f93d598d
Wilfred <me@wilfred.me.uk>
2010-10-31 09:42:08 +0000

    reducing html duplication using template inheritance

--- settings.py
+++ settings.py
@@ -13,12 +13,12 @@ PROJECT_DIR = os.path.dirname(__file__)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # {postgresql_psycopg2,postgresql,mysql,sqlite3,oracle}
-        'NAME': '/home/wilfred/html/projektoj/vortaro/word_db', # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/home/wilfred/html/projektoj/vortaro/word_db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -76,6 +76,7 @@ ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_DIR, "templates"),
+    os.path.join(PROJECT_DIR, "templates/vortaro"),
 )
 
 INSTALLED_APPS = (
