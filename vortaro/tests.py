99cb2b6423fdfbc1c81d231b88c67e6aea44ba64
Wilfred Hughes <me@wilfred.me.uk>
2014-09-11 22:56:03 +0100

    Adding a basic test of our index page.

---
+++ vortaro/tests.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+class IndexTests(TestCase):
+    def test_index_renders(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
