547ff5ec607c724be3d1e7fca473514c04741e92
Wilfred Hughes <me@wilfred.me.uk>
2014-09-12 19:58:14 +0100

    URL refactoring.

--- urls.py
+++ urls.py
@@ -7,12 +7,12 @@ static_files_path = os.path.join(settings.PROJECT_DIR, "static")
 
 if settings.DEBUG:
     # serve static files using Django
-    urlpatterns = patterns('',
-        url(r'^informo$', 'vortaro.views.about', name="about"),
+    urlpatterns = patterns('vortaro.views',
+        url(r'^informo$', 'about', name="about"),
         # Django excludes GET arguments from the URL, so just match ""
-        url(u'^$', 'vortaro.views.index', name="index"),
+        url(u'^$', 'index', name="index"),
         (r'^resources/(?P<path>.*)$', 'django.views.static.serve', {'document_root': static_files_path}))
 else:
-    urlpatterns = patterns('',
-        url(r'^informo$', 'vortaro.views.about', name="about"),
-        url(u'^$', 'vortaro.views.index', name="index"))
+    urlpatterns = patterns('vortaro.views',
+        url(r'^informo$', 'about', name="about"),
+        url(u'^$', 'index', name="index"))
