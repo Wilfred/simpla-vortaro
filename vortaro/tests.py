e1b2ca926caa7a61e440034a112c22a808c1e6e3
Wilfred Hughes <me@wilfred.me.uk>
2014-09-12 19:48:05 +0100

    Adding another basic unit test.

--- vortaro/tests.py
+++ vortaro/tests.py
@@ -5,3 +5,9 @@ class IndexTests(TestCase):
     def test_index_renders(self):
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
+
+
+class AboutTests(TestCase):
+    def test_about_renders(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
